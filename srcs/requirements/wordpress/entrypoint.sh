f96406bd90de59cec681444465506e9e42436b40
inception vm <efwef@gmail.com>
2023-08-01 15:51:05 +0200

    add .env

---
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,17 @@
+cd /var/www/html/wordpress
+
+wp core download  --path="/var/www/html/wordpress" --allow-root
+
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
+
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+wp plugin update --allow-root --all
+wp user create --path="/var/www/html/wordpress" --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
+
+# wp theme install neve --activate --allow-root
+
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+
+mkdir -p /run/php/
+php-fpm7.3 -F 
